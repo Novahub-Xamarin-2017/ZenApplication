7930d7bd1632a3daf8bb740e5628158dfa5c379b
Mẫn <vanman.lqd@gmail.com>
2018-03-26 10:13:01 +0700

    Init project

---
+++ ZenApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ZenApp
+{
+    [Activity(Label = "ZenApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
